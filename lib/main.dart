2f41ad4b13120ad6fa50c8248506c96b6073f8a1
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-03-01 03:39:12 +0700

    login screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/view/screen/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Scaffold(),
+      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/view/screen/auth/login_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/view/widget/text_input_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _sandiController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Titkok Clone',
+            style: TextStyle(
+              fontSize: 35,
+              color: buttonColor,
+              fontWeight: FontWeight.w900,
+            ),
+          ),
+          const Text(
+            'Login',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextInputField(
+              controller: _emailController,
+              labelText: 'Email',
+              icon: Icons.email,
+            ),
+          ),
+          const SizedBox(height: 25),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: TextInputField(
+              controller: _sandiController,
+              labelText: 'Kata Sandi',
+              icon: Icons.lock,
+            ),
+          ),
+          const SizedBox(height: 30),
+          Container(
+            width: MediaQuery.of(context).size.width - 40,
+            height: 50,
+            decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(5),
+              ),
+            ),
+            child: InkWell(
+              onTap: () {
+                debugPrint('login user');
+              },
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Belum mempunyai akun ? ',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  debugPrint('navigate ke register page');
+                },
+                child: Text(
+                  'Daftar',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: buttonColor,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+}
